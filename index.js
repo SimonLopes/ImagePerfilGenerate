af7d589a8ea7769e654b2d210892d9468b3e9f53
Simon Kann <simonlopes99@gmail.com>
2020-05-28 17:50:28 -0300

    configuracoes heroku

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const bodyParser = require("body-parser")
+const cors = require('cors')
 
 const robots = {
     image: require("./robot/image.js"),
@@ -11,6 +12,7 @@ const {_PORT, _IMAGEPATH} = {
     "_IMAGEPATH":"content"
 }
 
+app.use(cors())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.use('/content', express.static('./content'))
