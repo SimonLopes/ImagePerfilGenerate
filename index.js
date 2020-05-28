371ce5be23839644eaa160d9cb7dff2027eed9f4
Simon Kann <simonlopes99@gmail.com>
2020-05-28 15:23:02 -0300

    Primeiro commit, projeto gerador de imagens de perfil de usuario

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const app = express()
+const bodyParser = require("body-parser")
+
+const robots = {
+    image: require("./robot/image.js"),
+}
+
+const {_PORT, _IMAGEPATH} = {
+    "_PORT":"3000",
+    "_IMAGEPATH":"content"
+}
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+app.use('/content', express.static('./content'))
+
+app.post(["/", "/index"], async (req, res) => {
+    var {userName, fileName, width, height} = req.body
+
+    var initialsLetters = userName.split(' ')[0].charAt(0) + userName.split(' ')[1].charAt(0)
+
+    var savingFileName = await robots.image(initialsLetters, fileName, width, height)
+
+    if(savingFileName.status == true){
+        res.send(`${req.protocol}://${req.get('host')}/${_IMAGEPATH}/${savingFileName.return}`)
+    }else{
+        res.send({'Status': false, 'msg': 'Ocorreu um erro durante a execucao', 'error-description': savingFileName.return})
+    }
+
+});
+
+app.listen(_PORT, function() {
+    console.log(`App escutando na porta ${_PORT}!`)
+})
---
+++ robot/image.js
@@ -0,0 +1,156 @@
+const { createCanvas } = require('canvas')
+const fs = require('fs')
+
+async function robot(
+    textUser,
+    fileName = textUser,
+    width = 512,
+    height = 512,
+    bgColor = randColor(),
+    textColor = randColorText(randColor(), lightOrDark(bgColor)),
+    textSize = width/2,
+    
+) {
+
+    var canvas = await generateImage(textUser, bgColor, textColor, textSize, width, height)
+    return await saveImage(fileName, canvas)
+}
+
+function generateImage(text, bgColor, textColor, textSize, w, h) {
+    var canvas = createCanvas(w, h)
+    var context = canvas.getContext('2d')
+    
+    context.fillStyle = bgColor
+    context.fillRect(0, 0, w, h)
+    
+    context.font = `bold ${textSize}pt Arial`
+    context.textAlign = 'center'
+    context.textBaseline = 'middle'
+    context.fillStyle = textColor
+    context.shadowColor = "rgba(0,0,0,1)";
+    context.shadowBlur = 9;
+    context.fillText(text, w/2, h/2)
+
+    return canvas
+}
+
+function saveImage(name, canvas) {
+    try {
+        var outputFileName = `${name}-${Math.floor(Date.now() / 1000)}.png`
+        var buffer = canvas.toBuffer('image/png')
+        fs.writeFileSync(`./content/${outputFileName}`, buffer)
+        return {'status': true, 'return': outputFileName}
+    } catch (err) {
+        return {'status': false, 'return': err}
+    }
+    
+}
+
+function lightOrDark(color) {
+    if (color.match(/^rgb/)) {
+        color = color.match(/^rgba?\((\d+),\s*(\d+),\s*(\d+)(?:,\s*(\d+(?:\.\d+)?))?\)$/)
+        r = color[1]
+        g = color[2]
+        b = color[3]
+    } 
+    else {
+        color = +("0x" + color.slice(1).replace( 
+            color.length < 5 && /./g, '$&$&'
+            )
+        );
+        r = color >> 16
+        g = color >> 8 & 255
+        b = color & 255
+    }
+    var hsp = Math.sqrt(
+        0.299 * (r * r) +
+        0.587 * (g * g) +
+        0.114 * (b * b)
+    )
+    return hsp > 127.5 ? 'light' : 'dark'
+}
+
+function randColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+function randColorText(textColor, bgBright) {
+    var pad = function(num, totalChars) {
+        var pad = '0'
+        num = num + ''
+        while (num.length < totalChars) {
+            num = pad + num
+        }
+        return num
+    }
+    
+    var changeColor = function(color, ratio, darker) {
+        color = color.replace(/^\s*|\s*$/, '')
+        color = color.replace(
+            /^#?([a-f0-9])([a-f0-9])([a-f0-9])$/i,
+            '#$1$1$2$2$3$3'
+        )
+    
+        var difference = Math.round(ratio * 256) * (darker ? -1 : 1),
+            rgb = color.match(new RegExp('^rgba?\\(\\s*' +
+                '(\\d|[1-9]\\d|1\\d{2}|2[0-4][0-9]|25[0-5])' +
+                '\\s*,\\s*' +
+                '(\\d|[1-9]\\d|1\\d{2}|2[0-4][0-9]|25[0-5])' +
+                '\\s*,\\s*' +
+                '(\\d|[1-9]\\d|1\\d{2}|2[0-4][0-9]|25[0-5])' +
+                '(?:\\s*,\\s*' +
+                '(0|1|0?\\.\\d+))?' +
+                '\\s*\\)$'
+            , 'i')),
+            alpha = !!rgb && rgb[4] != null ? rgb[4] : null,
+            decimal = !!rgb? [rgb[1], rgb[2], rgb[3]] : color.replace(
+                /^#?([a-f0-9][a-f0-9])([a-f0-9][a-f0-9])([a-f0-9][a-f0-9])/i,
+                function() {
+                    return parseInt(arguments[1], 16) + ',' +
+                        parseInt(arguments[2], 16) + ',' +
+                        parseInt(arguments[3], 16)
+                }
+            ).split(/,/),
+            returnValue
+        return !!rgb ?
+            'rgb' + (alpha !== null ? 'a' : '') + '(' +
+                Math[darker ? 'max' : 'min'](
+                    parseInt(decimal[0], 10) + difference, darker ? 0 : 255
+                ) + ', ' +
+                Math[darker ? 'max' : 'min'](
+                    parseInt(decimal[1], 10) + difference, darker ? 0 : 255
+                ) + ', ' +
+                Math[darker ? 'max' : 'min'](
+                    parseInt(decimal[2], 10) + difference, darker ? 0 : 255
+                ) +
+                (alpha !== null ? ', ' + alpha : '') +
+                ')' :
+            [
+                '#',
+                pad(Math[darker ? 'max' : 'min'](
+                    parseInt(decimal[0], 10) + difference, darker ? 0 : 255
+                ).toString(16), 2),
+                pad(Math[darker ? 'max' : 'min'](
+                    parseInt(decimal[1], 10) + difference, darker ? 0 : 255
+                ).toString(16), 2),
+                pad(Math[darker ? 'max' : 'min'](
+                    parseInt(decimal[2], 10) + difference, darker ? 0 : 255
+                ).toString(16), 2)
+            ].join('')
+    }
+    var lighterColor = function(color, ratio) {
+        return changeColor(color, ratio, false)
+    };
+    var darkerColor = function(color, ratio) {
+        return changeColor(color, ratio, true)
+    };
+    
+    return bgBright == 'dark' ? lighterColor(textColor, .2) : darkerColor(textColor, .2)
+}
+
+module.exports = robot
